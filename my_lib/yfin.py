da5783b456eaa082f3b862e99b47571289b23ae2
Aloysius Pious <33965547+AloysiusPious@users.noreply.github.com>
2024-04-09 12:38:25 +0530

    Delete my_lib directory

--- my_lib/yfin.py
+++
@@ -1,7 +0,0 @@
-import yfinance as yf
-def get_df_from_yf(symbol, start_date, end_date):
-    #df = yf.download(symbol, start=start_date, end=end_date)
-    yf_data = yf.download(symbol + '.NS', start_date, end_date)
-    # Convert to pandas DataFrame
-    df = yf_data.copy()
-    return df
